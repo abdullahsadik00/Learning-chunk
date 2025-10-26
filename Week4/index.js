f01795443763e184f1c3fe679f4f640d7131089f
Sadik Shaikh <abdullahsadik00@gmail.com>
2025-10-26 14:02:09 +0530

    simple cli to count the word

--- Week4/index.js
+++ Week4/index.js
@@ -1,14 +1,29 @@
 const fs = require('fs');
+const { Command } = require('commander');
+const program = new Command();
+
+program.name('word-counter')
+    .description('Counts the number of words in a text file')
+    .version('1.0.0');
+
+program.command('count')
+    .description('Count words in the specified file')
+    .argument('<file>', 'Path to the text file')
+    .action((file) => {
+        console.log("Counting words in file:", file);
+        main(file);
+    });
+
 function main(filePath) {
-    console.log();
+    console.log('Reading file:', filePath);
     fs.readFile(filePath, 'utf8', (err, data) => {
-        var len = data.split(' ').length
+
         if (err) {
             console.error("Error reading file:", err);
             return;
         }
+        var len = data.split(' ').length
         console.log("This file contain", len, "words");
     });
 }
-
-main(process.argv[2]);
+program.parse(process.argv);
