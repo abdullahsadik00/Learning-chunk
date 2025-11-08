559dc13370c50789cebb3fe18d7ecfdc4aedacfc
Sadik Shaikh <abdullahsadik00@gmail.com>
2025-11-08 18:53:15 +0530

    rate limitter

--- Week4/middlewares/01-ratelimitter.js
+++ Week4/middlewares/01-ratelimitter.js
@@ -15,6 +15,20 @@ let numberOfRequestsForUser = {};
 setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
+app.use((req, res, next) => {
+  const userId = req.headers["user-id"];
+    if(numberOfRequestsForUser[userId] > 5) {
+      res.status(404).send("No Entry!");
+    }else {
+      if(numberOfRequestsForUser[userId]) {
+        numberOfRequestsForUser[userId]++;
+      } else {
+        numberOfRequestsForUser[userId] = 1;
+      }
+      next();
+    }
+
+});
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
