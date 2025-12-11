8cd19e7a18d6fc174abdd39cdffefc012d096785
Sadik Shaikh <abdullahsadik00@gmail.com>
2025-12-11 20:50:04 +0530

    Initialize async_nodejs project with Express and basic server setup

---
+++ Week4/async_nodejs/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = express.Router();
+
+// Middleware to parse JSON bodies
+const app = express();
+app.use(bodyParser.json());
+app.use(routes)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const PORT = 3000;
+
+// Sample route
+routes.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
