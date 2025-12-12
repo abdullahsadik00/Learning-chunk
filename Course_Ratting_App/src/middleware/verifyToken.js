9ef94a7803e6df2ea7752013c3415e3f8976decd
Sadik Shaikh <abdullahsadik00@gmail.com>
2025-12-12 19:25:02 +0530

    Add token verification middleware and protect course creation route

---
+++ Course_Ratting_App/src/middleware/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const User = require('../model/user');
+
+const verifyToken = (req, res, next) => {
+    if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+
+        jwt.verify(req.headers.authorization.split(' ')[1], 'SECRET_KEY', (err, decode) => {
+            if (err) {
+                req.user = undefined;
+                next();
+            } else {
+                User.findById(decode.id).then(user => {
+                    req.user = user;
+                    next();
+                }).catch(err => {
+                    res.status(500).json({ hasError: true, message: "Error fetching user", error: err });
+                });
+            }
+        });
+    } else {
+        req.user = undefined;
+        req.message = "Authorization header not found";
+        next();
+    }
+}
+
+module.exports = verifyToken;
